c7e19465110a2106e98e98c5b7d93457f44e4269
hermanrono-hue <hermanrono01@gmail.com>
2025-12-23 20:02:21 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// fom select
+const form = document.querySelector('#create-task-form');
+const taskList = document.querySelector('#tasks');
+
+// submit listener
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // Prevent page reload
+
+  //form data
+  const input = document.querySelector('#new-task-description');
+  const task = input.value.trim(); // spaces del
+
+  if (task !== '') {
+    buildToDo(task); //todo
+    input.value = ''; // Clear the input after adding
+  }
 });
+//item add
+function buildToDo(task) {
+  const li = document.createElement('li');
+  li.textContent = task;
+  taskList.appendChild(li);
+}
